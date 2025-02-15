776ab2964aae217793c20554281f70cc07bbb751
ivancekikj <ivan.cekic10@gmail.com>
2025-02-15 23:21:30 +0100

    Generic page structure, add bootstrap

---
+++ src/lib/view/pages/GenericPage.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from '$lib/view/components/Header.svelte';
+	import Footer from '$lib/view/components/Footer.svelte';
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/css/style.css" />
+</svelte:head>
+
+<Header />
+<slot></slot>
+<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import GenericPage from '$lib/view/pages/GenericPage.svelte';
+	import HomePage from '$lib/view/pages/HomePage.svelte';
+</script>
+
+<GenericPage>
+	<HomePage></HomePage>
+</GenericPage>
